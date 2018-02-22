8e49ef3a01115ce095111593b4e09f96446179f7
neilcar <neil@twistlock.com>
2018-02-22 08:03:27 -0500

    Update compliances.ps1

--- powershell/compliances.ps1
+++ powershell/compliances.ps1
@@ -1,26 +1,21 @@
-# We want to call the container API to get all
-# of the runtime models
+# We want to call the images API
+# The search parameter ("?search=<image_name>") is optional
 $request = "https://console:8083/api/v1/images?search=<image_name>"
+
 # We will need credentials to connect so we will ask the user
-# $cred = Get-Credential
+$cred = Get-Credential
 
-# Step 1. Create a username:password pair
-$credPair = "admin:Passwordhere"
-# Step 2. Encode the pair to Base64 string
-$encodedCredentials = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($credPair))
-# Step 3. Form the header and add the Authorization attribute to it
-$headers = @{ Authorization = "Basic $encodedCredentials" }
-
-  # Get the JSON data back from Twistlock and turn it
+# Get the JSON data back from Twistlock and turn it
 # into a PowerShell object.
-$images = Invoke-RestMethod $request -Headers $headers -UseBasicParsing
+$images = Invoke-RestMethod $request -Authentication Basic -Credential $cred
 
-
-#$images = Invoke-RestMethod $request -Authentication Basic -Credential $cred
-# Answer the question 
+# Iterate through all the images returned 
 foreach($image in $images)
 {
+    # For each image, output the image ID and the 
     $image._id
     $image.instances.image
+    
+    # Output compliance issues for this image in a table
     $image.info.complianceVulnerabilities | Select-Object id, severity, cvss, description
 }
