fd6b8426f046df52067b57866171c67f5ee5bba8
matthewabq <matthew@twistlock.com>
2019-04-30 19:14:13 +0000

    removed space from folder

---
+++ CI/Azure_Devops/Extensions/azure-devops-twistcli-task/overview.md
@@ -0,0 +1,38 @@
+# Task for scanning container images and serverless functions in pipelines using twistcli
+
+## How to use
+The extension assumes that the `twistcli` command is available in the `PATH` (e.g. `usr/bin`) and executable. You can use a separate task (e.g. Universal Package download) to download it into your Azure DevOps Pipeline.
+
+```
+# Example for Command Line Task after Universal Download
+sudo mv $(System.DefaultWorkingDirectory)/twistcli /usr/bin/twistcli
+sudo chmod +x /usr/bin/twistcli
+```
+
+ We've made `twistcli` pluggable by design so there's no need to uninstall/reinstall the Twistlock extension from your organizations/projects/pipelines for each new Twistlock version upgrade, simply upload the updated `twistcli` binary to your feed and select that version to be used in your pipeline.
+
+---
+**NOTE**
+
+The `twistcli` version needs to match the Twistlock Console version.
+
+---
+
+## Available tasks
+
+Azure DevOps
+
+There is a single task **Twistlock twistcli scan** which scans a Docker container image or serverless function bundle zip file, displays the results locally, and sends them to the Twistlock Console.
+
+## Get the source
+
+The [source](https://github.com/twistlock/sample-code/tree/master/CI/Azure%20DevOps/Extensions/azure-devops-twistcli-task) for this extension is on GitHub.
+
+## Contribute
+
+This extension was created by Mario Weigel and is now stewarded by Twistlock and the Twistlock user community.
+
+## Feedback and issues
+
+If you have feedback or issues, please file an issue on [GitHub](https://github.com/twistlock/sample-code/issues)
+
---
+++ CI/Azure_Devops/Extensions/azure-devops-twistcli-task/tasks/twistcli-scan/task.ts
@@ -0,0 +1,103 @@
+import fs = require("fs")
+import path = require("path")
+import tl = require('azure-pipelines-task-lib/task');
+import tr = require('azure-pipelines-task-lib/toolrunner');
+
+// Simple wrapper around Twistlock's twistcli tool.
+async function run() {
+    try {
+        // Get Twistlock service endpoint configuration. This inclues the URL of the Twistlock console, the username
+        // and password used to authenticate twistcli to the console and the certificate of the CA that issued the
+        // console's certificate so that it can be verified.
+        //
+        // https://docs.microsoft.com/en-us/azure/devops/pipelines/library/service-endpoints?view=azure-devops
+        let twistlockServiceEndpoint: string = tl.getInput('twistlockService', true);
+        const twistlockServiceEndpointAuthorization = tl.getEndpointAuthorization(twistlockServiceEndpoint, false);
+
+        // Construct twistcli command line.
+        let twistcliPath: string = tl.which('twistcli', true);
+        let twistcli = tl.tool(twistcliPath);
+        if (tl.getInput('scanType', true) === "serverless") {
+          twistcli.arg("serverless");
+        } else {
+          twistcli.arg("images");
+        }
+        twistcli.arg("scan");
+
+        // Set console address.
+        if (!tl.getEndpointUrl(twistlockServiceEndpoint, true).startsWith('https://')) {
+            throw 'The Twistlock console must be accessed via https'
+        } else {
+            twistcli.arg(['--address', tl.getEndpointUrl(twistlockServiceEndpoint, true)]);
+        }
+
+        // If supplied, write the CA certificate received from the DevOps service connection configuration
+        // to a temporary location so its path can be passed as an option to twistcli.
+        if (twistlockServiceEndpointAuthorization.parameters.tlscacert != '') {
+            let caCertPath = writeCACertificate(twistlockServiceEndpointAuthorization.parameters.tlscacert)
+            twistcli.arg(['--tlscacert', caCertPath]);
+        } else {
+            tl.warning('No CA certificate supplied in Twistlock console service connection');
+            tl.warning('The connection to the Twistlock console is insecure');
+        }
+
+        // Set vulnerability configuration.
+        twistcli.arg(['--vulnerability-threshold', tl.getInput('vulnerabilityThreshold', true)]);
+        // --grace-period is only for container images
+        if (tl.getInput('scanType', true) === "images") {
+          twistcli.arg(['--grace-period', tl.getInput('gracePeriod', true)]);
+        }
+
+        if (tl.getBoolInput('onlyFixed', true)) {
+            twistcli.arg('--only-fixed');
+        }
+
+        // Set compliance configuration.
+        twistcli.arg(['--compliance-threshold', tl.getInput('complianceThreshold', true)]);
+
+        // Set Twistlock console authentication configuration.
+        twistcli.arg(['--user', twistlockServiceEndpointAuthorization.parameters.username]);
+        twistcli.arg(['--password', twistlockServiceEndpointAuthorization.parameters.password]);
+
+        // Show detailed vulnerability and compliance information.
+        twistcli.arg('--details');
+
+        // Set the image or functions zip to be scanned.
+        twistcli.arg(tl.getInput('artifact', true));
+
+        // Execute twistcli.
+        let exitCode: number = await twistcli.exec(<tr.IExecOptions>{
+            cwd: ".",
+            failOnStdErr: false,
+            errStream: process.stdout,
+            outStream: process.stdout,
+            ignoreReturnCode: true,
+            env: {},
+            silent: false,
+            windowsVerbatimArguments: false
+        });
+
+        if (exitCode == 0) {
+            tl.setResult(tl.TaskResult.Succeeded, '');
+        } else {
+            tl.setResult(tl.TaskResult.Failed, 'Either scanning failed or the scan results do not satisfy policy. Check the logs for details.');
+        }
+    }
+    catch (err) {
+        tl.setResult(tl.TaskResult.Failed, err);
+    }
+}
+
+// Write the CA certificate received from the DevOps service connection configuration
+// to a temporary file.
+function writeCACertificate(caCertData: string): string {
+    let tempDirectory = tl.getVariable('agent.tempDirectory');
+    tl.checkPath(tempDirectory, `${tempDirectory} (agent.tempDirectory)`);
+
+    let caCertPath = path.join(tempDirectory, 'ca-cert.pem');
+    fs.writeFileSync(caCertPath, caCertData);
+
+    return caCertPath;
+}
+
+run();
