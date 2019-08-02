9fce2ef13b11145af32999cf245c0cb931b2979d
Jeremy Adams <3187222+jpadams@users.noreply.github.com>
2019-08-01 22:07:06 -0700

    Update to Docker 19.03

--- onebox-install-examples/ubuntu-docker-onebox.sh
+++ onebox-install-examples/ubuntu-docker-onebox.sh
@@ -16,7 +16,7 @@ sudo apt-get install -y apt-transport-https ca-certificates curl software-proper
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
 sudo apt-get update
-sudo apt-get install -y docker-ce=$(apt-cache madison docker-ce | grep 18.03 | head -1 | awk '{print $3}')
+sudo apt-get install -y docker-ce=$(apt-cache madison docker-ce | grep 19.03 | head -1 | awk '{print $3}')
 
 echo ‘########################’
 
