19d06e837b3c18ce968890cd4baaa3d0d214decf
Stephen Gordon <stephen@twistlock.com>
2019-12-21 08:02:32 -0600

    update to remove certificate warnings

--- powershell/install_container_defender.ps1
+++ powershell/install_container_defender.ps1
@@ -8,6 +8,8 @@ $console = 'https://<console-addr>:8083'
 $request = $console + '/api/v1/authenticate'
 
 # Initial request for token and save into variable
+add-type "using System.Net; using System.Security.Cryptography.X509Certificates; public class TrustAllCertsPolicy : ICertificatePolicy{ public bool CheckValidationResult(ServicePoint srvPoint, X509Certificate certificate, WebRequest request, int certificateProblem) { return true; }}"; [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy;
+
 $response = Invoke-WebRequest $request  -Body ($params|ConvertTo-Json) -ContentType "application/json" -Method POST
 $token =  ConvertFrom-Json $([String]::new($response.Content))
 
@@ -16,3 +18,5 @@ $request2 = $console + '/api/v1/scripts/defender.ps1'
 Invoke-WebRequest $request2 -Headers @{"authorization" = 'Bearer ' + $token.token } -OutFile defender.ps1;
 
 # file will be saved as defender.ps1 in the current directory
+
+
