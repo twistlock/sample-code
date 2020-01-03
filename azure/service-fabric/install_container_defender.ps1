776ec3c7646a52c0fd05f86c46953aa129258629
Stephen Gordon <stephen@twistlock.com>
2020-01-03 16:32:40 -0600

    Update with install commands

--- azure/service-fabric/install_container_defender.ps1
+++ azure/service-fabric/install_container_defender.ps1
@@ -19,4 +19,10 @@ Invoke-WebRequest $request2 -Headers @{"authorization" = 'Bearer ' + $token.toke
 
 # file will be saved as defender.ps1 in the current directory
 
+#remove docker engine prerequisite
+(Get-Content -path defender.ps1) -replace ' -DependsOn "docker"' | Set-Content -Path defender.ps1
+
+#Install Twistlock
+.\defender.ps1 -type dockerWindows -consoleCN <console-addr> -install
+
 
