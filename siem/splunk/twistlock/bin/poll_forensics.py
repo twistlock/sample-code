433af098f69c98e9ff9da814ffa751acdd5fd6ce
mwilco03 <36248105+mwilco03@users.noreply.github.com>
2021-03-01 20:53:29 -0500

    Update poll_forensics.py

--- siem/splunk/twistlock/bin/poll_forensics.py
+++ siem/splunk/twistlock/bin/poll_forensics.py
@@ -1,61 +1,73 @@
 from __future__ import print_function
 import json
 import os
-import requests
 import sys
-
+import requests
+try:
+    from urllib.parse import urljoin
+except ImportError:
+    from urlparse import urljoin
 from api_wrappers import get_auth_token
 
 
 data_dir = os.path.join(os.environ["SPLUNK_HOME"], "etc", "apps", "twistlock", "bin", "data")
-
 # config_file - stores the Console URL and authentication information
 config_file = os.path.join(data_dir, "config.json")
 
-forensics_file = os.path.join(data_dir, "forensics_events.txt")
+incidents_file = os.path.join(data_dir, "incidents_list.txt")
 
 def get_forensics(console_url, auth_token):
     endpoint = "/api/v1/profiles/"
     headers = {"Authorization": "Bearer " + auth_token, "Accept": "application/json"}
-    request_limit = 500
+    request_limit = 100
 
-    with open(forensics_file) as f:
-        field_extracts = json.load(f)
-    
-    for field in field_extracts:
-        params = {"project": field["project"], "limit": request_limit, "incidentID": field["_id"]}
-        params_string = "&".join("%s=%s" % (k,v) for k,v in params.items())
-        url = console_url + endpoint + field["type"] + "/" + field["profileID"] + "/forensic"
-        try:
-            response = requests.get(url, params=params_string, headers=headers)
-            response.raise_for_status()
-            response_json = response.json()
-        except (requests.exceptions.RequestException, ValueError) as req_err:
-            print("Failed getting forensics\nincidentID: {}\nprofileID: {}\n{}".format(field['_id'], field['profileID'], req_err), file=sys.stderr)
-            continue
+    with open(incidents_file, "r+") as f:
+        incidents = json.load(f)
+        while incidents:
+            incident = incidents.pop(0)
+            params = {"project": incident["project"], "limit": request_limit, "incidentID": incident["_id"]}
+            params_string = "&".join("%s=%s" % (k,v) for k,v in params.items())
+            api_path = endpoint + incident["type"] + "/" + incident["profileID"] + "/forensic"
+            if console_url == incident["url"]:
+                request_url = urljoin(console_url, api_path)
+                try:
+                    response = requests.get(request_url, params=params_string, headers=headers, verify=False, timeout=0.25)
+                    response.raise_for_status()
+                    response_json = response.json()
+                except (requests.exceptions.RequestException, ValueError) as req_err:
+                    print("Failed getting forensics\nincidentID: {}\nprofileID: {}\n{}".format(incident["_id"], incident["profileID"], req_err), file=sys.stderr)
+                    continue
 
-        if response_json is not None:
-            # Add incident ID to forensic data
-            for element in response_json:
-                element["incidentID"] = field["_id"]
-                print(json.dumps(element))
+                if response_json is not None:
+                    # Add incident ID to forensic data
+                    for forensic in response_json:
+                        forensic["console"] = console_url
+                        forensic["incidentID"] = incident["_id"]
+                        print(json.dumps(forensic))
 
-    os.remove(forensics_file)
+                # Keep file up-to-date with unprocessed incidents
+                f.truncate(0)
+                f.seek(0)
+                json.dump(incidents, f)
+                f.flush()
 
 
 if __name__ == "__main__":
-    if (os.path.isfile(forensics_file)):
+    if (os.path.isfile(incidents_file)):
         config = json.load(open(config_file))
 
-        if not (config["console"]["url"] and config["credentials"]["username"] and config["credentials"]["password"]):
+        if not (config["consoles"][0]["url"] and config["consoles"][0]["credentials"]["username"] and config["consoles"][0]["credentials"]["password"]):
             print("At least one item is missing in config.json", file=sys.stderr)
             sys.exit(1)
-        
-        username = config["credentials"]["username"]
-        password = config["credentials"]["password"]
-        console_url = config["console"]["url"]
+        for console in range(0,len(config["consoles"])):
+            username = config["consoles"][console]["credentials"]["username"]
+            password = config["consoles"][console]["credentials"]["password"]
+            console_url = config["consoles"][console]["url"]
+            auth_token = get_auth_token(console_url, username, password)
+            get_forensics(console_url, auth_token)
 
-        auth_token = get_auth_token(console_url, username, password)["token"]
-        get_forensics(console_url, auth_token)
+        os.remove(incidents_file)
     else:
-        print("Forensics file not found", file=sys.stderr)
+        print("Incidents file not found", file=sys.stderr)
+
+
